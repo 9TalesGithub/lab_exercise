ac6cb606b491efccb593eb1c4ca2675a4b5aca42
9TalesGithub <112519255+9TalesGithub@users.noreply.github.com>
2023-02-13 16:26:13 -0600

    Put the files in here

---
+++ functions.js
@@ -0,0 +1,10 @@
+var num = 0;
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World!";
+}
+
+function button1() {
+    num++;
+    document.getElementById("output").innerHTML = "Press x"+num;
+}
